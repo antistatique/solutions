63bf96ec3a613271a46ba527bd1b50aedd257c1c
Gilles Doge <gilles.doge@gmail.com>
2012-07-11 12:06:06 +0200

    add Form

--- src/controllers.php
+++ src/controllers.php
@@ -5,6 +5,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+$contactForm = $app['form.factory']->createBuilder('form')
+    ->add('name', 'text', array(
+        'label' => 'Nom',
+        'constraints' => array(new Assert\NotBlank()),
+    ))
+    ->add('email', 'email', array(
+        'label' => 'Email',
+        'constraints' => new Assert\Email(),
+    ))
+    ->add('phone', 'text', array(
+        'label' => 'Téléphone',
+    ))
+    ->add('message', 'textarea', array(
+        'label' => 'Message',
+        'required' => false,
+    ))
+    ->add('subject', 'hidden', array(
+        'constraints' => array(new Assert\NotBlank()),
+    ))
+    ->getForm()
+;
 
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html', array());
